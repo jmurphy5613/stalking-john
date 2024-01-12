6cad31c6419284a19f1d5151a2b1a8af34f2204d
jmurphy5613 <jmurphy5613@gmail.com>
2024-01-12 15:56:50 -0500

    set up proj

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import exp from "constants"
+import styles from './Navbar.module.css'
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+export default Navbar
